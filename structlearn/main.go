5b2446761279e0e739102021ccc119709e4dace7
noel zhang <noel.zhang@yuanqu-tech.com>
2022-04-24 17:01:47 +0800

    结束结构体

--- structlearn/main.go
+++ structlearn/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"structlearn/book"
 	"fmt"
+	"structlearn/book"
 )
 
-func main()  {
+//显式初始化，
+//不建议使用
+// var book2 = book.Book{"The Go Program Language",700,make(map[string]int)}
+//建议使用
+var book3 = book.Book{
+	Title:"The Go Program Language",
+	Pages: 20,
+	Indexs: make(map[string]int),
+}
+
+
+
+func main() {
 	var book = book.Book{}
 	book.Title = "The Go Programming Language"
 	book.Pages = 34
